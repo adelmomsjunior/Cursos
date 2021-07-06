809aad793447a1cb75b813c9721ca9d150e14a04
theadelmojr <adelmomsjunior@outlook.com>
2021-07-06 19:28:29 -0300

    solucao

--- .NETfundamentals/Solução de problemas com .NET/desafio1/Program.cs
+++ .NETfundamentals/Solução de problemas com .NET/desafio1/Program.cs
@@ -1,4 +1,36 @@
-﻿using System;
+﻿/*Programa para Validação de Notas
+
+O calendário escolar está passando bem rápido, devido a isso, as professoras
+de uma escola estão com dificuldade para calcular as notas dos alunos. Visando 
+em resolver a situação, a diretora da escola contratou você para desenvolver 
+um programa que leia as notas da primeira e da segunda avaliação de um aluno. 
+Calcule e imprima a média semestral.
+O programa só aceitará notas válidas (uma nota válida deve pertencer ao intervalo [0,10]). 
+Cada nota deve ser validada separadamente.
+No final deve ser impressa a mensagem “novo calculo (1-sim 2-nao)”, solicitando 
+as professoras que informe um código (1 ou 2) indicando se ele deseja ou não 
+executar o algoritmo novamente, (aceitar apenas os código 1 ou 2). Se for 
+informado o código 1 deve ser repetida a execução de todo o programa para permitir 
+um novo cálculo, caso contrário o programa deve ser encerrado.
+
+Entrada
+O arquivo de entrada contém vários valores reais, positivos ou negativos. 
+Quando forem lidas duas notas válidas, deve ser lido um valor inteiro X . 
+O programa deve parar quando o valor lido para este X for igual a 2.
+
+Saída
+Se uma nota inválida for lida, deve ser impressa a mensagem “nota invalida”. 
+Quando duas notas válidas forem lidas, deve ser impressa a mensagem “media = ” 
+seguido do valor do cálculo.
+
+Antes da leitura de X deve ser impressa a mensagem "novo calculo (1-sim 2-nao)" 
+e esta mensagem deve ser apresentada novamente se o valor da entrada padrão para 
+X for menor do que 1 ou maior do que 2, conforme o exemplo abaixo.
+
+A média deve ser impressa com dois dígitos após o ponto decimal.*/
+
+using System;
+using System.Collections.Generic;
 
 namespace desafio1
 {
@@ -6,7 +38,43 @@ namespace desafio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double op = 0, nota;
+            double media, dec = 5.00;
+            do
+            {
+                List<double> duasnotas = new List<double>(); 
+                while(duasnotas.Count != 2)
+                {
+                    try
+                    {
+                        nota = double.Parse(Console.ReadLine());
+                        if (nota < 0 || nota > 10) 
+                        {
+                            Console.WriteLine("nota invalida");
+                        }
+                        else
+                        {
+                            duasnotas.Add(nota);
+                        }
+                        
+                    }
+                    catch
+                    {
+                        Console.WriteLine("nota invalida");
+                    }
+                }
+                media = (duasnotas[0] + duasnotas[1]) / duasnotas.Count;
+                Console.WriteLine("media = {0}", media.ToString("F2"));
+                do
+                {
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    op = int.Parse(Console.ReadLine());
+                    if(op == 1 || op == 2)
+                    {
+                        break;
+                    }
+                }while(op !=1 || op != 2);
+            }while(op!=2);
         }
     }
 }
--- .NETfundamentals/Solução de problemas com .NET/desafio3/Program.cs
+++ .NETfundamentals/Solução de problemas com .NET/desafio3/Program.cs
@@ -1,4 +1,34 @@
-﻿using System;
+﻿/* Fila do banco
+
+O banco que você trabalha sempre tem problemas para organizar as 
+filas de atendimento dos clientes.
+
+Após uma reunião com a gerência ficou decidido que os clientes ao 
+chegar na agência receberão uma senha numérica em seu aparelho de 
+celular via sms e que a ordem da fila será dada não pela ordem de 
+chegada, mas sim pelo número recebido via sms. A ordem de atendimento 
+será decrescente: aqueles que receberam número maior deverão ser 
+atendidos primeiro. 
+Então, dada a ordem de chegada dos clientes reordene a fila de acordo 
+com o número recebido via sms, e diga quantos clientes não precisaram 
+trocar de lugar nessa reordenação.
+
+Entrada
+A primeira linha contém um inteiro N, indicando o número de casos de 
+teste a seguir.
+Cada caso de teste inicia com um inteiro M (1 ≤ M ≤ 1000), indicando 
+o número de clientes. Em seguida haverá M inteiros distintos Pi (1 ≤ Pi ≤ 1000), 
+onde o i-ésimo inteiro indica o número recebido via sms do i-ésimo cliente.
+Os inteiros acima são dados em ordem de chegada, ou seja, o primeiro 
+inteiro diz respeito ao primeiro cliente a chegar na fila, o segundo 
+inteiro diz respeito ao segundo cliente, e assim sucessivamente.
+
+Saída
+Para cada caso de teste imprima uma linha, contendo um inteiro, indicando 
+o número de clientes que não precisaram trocar de lugar mesmo após a fila ser reordenada.*/
+
+using System;
+using System.Collections.Generic;
 
 namespace desafio3
 {
@@ -6,7 +36,28 @@ namespace desafio3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int fim = int.Parse(Console.ReadLine());
+            for(int i = 0; i < fim; i++)
+            {
+                List<double> lista = new List<double>{};
+                int cliente = int.Parse(Console.ReadLine());
+                string[] num = Console.ReadLine().Split();
+                for(int a=0; a<num.Length; a++)
+                {
+                    lista.Add(int.Parse(num[a]));
+                }
+                lista.Sort();
+                lista.Reverse();
+
+                for(int b=0; b<num.Length; b++)
+                {
+                    if(lista[b]!= int.Parse(num[b]))
+                    {
+                        cliente--;
+                    }
+                }
+                Console.WriteLine(cliente);
+            }                        
         }
     }
 }
